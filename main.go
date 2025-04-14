efdd0b7b04a20006a60e116dcc068fedf29498ec
uanhi <bingxio@qq.com>
2025-04-14 18:59:51 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type config struct {
+	port   string
+	device string
+}
+
+var conf config
+
+func init() {
+	port := flag.String("port", ":8080", "")
+	device := flag.String("device", "dbcfdab4", "")
+
+	flag.Usage = func() {
+		fmt.Println(`adbscp (Real-time ADB screenshot preview in browser)
+	
+Usage:
+  adbscp --port :8080 --device <serial>
+	
+Options:
+  --port <port>      port number to listen on (e.g. :8080)
+  --device <serial>  adb device serial number`)
+	}
+	flag.Parse()
+
+	conf.port = *port
+	conf.device = *device
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		flag.Usage()
+		return
+	}
+	exists, err := deviceExists(conf.device)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if !exists {
+		fmt.Printf("device not found %s\n", conf.device)
+		return
+	}
+	http.HandleFunc("/", Get)
+
+	if err := http.ListenAndServe(conf.port, nil); err != nil {
+		panic(err)
+	}
+}
+
+func deviceExists(serial string) (bool, error) {
+	cmd := exec.Command("adb", "devices")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	if err := cmd.Run(); err != nil {
+		return false, err
+	}
+	lines := strings.SplitSeq(out.String(), "\n")
+
+	for line := range lines {
+		part := strings.Split(line, "\t")
+		if part[0] == serial && part[1] == "device" {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/favicon.ico" {
+		http.NotFound(w, r)
+		return
+	}
+	err := exec.Command(
+		"adb",
+		"-s", conf.device,
+		"shell", "screencap", "-p", "/sdcard/screencap.png",
+	).Run()
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "screencap failed", http.StatusInternalServerError)
+		return
+	}
+	err = exec.Command(
+		"adb",
+		"-s", conf.device,
+		"pull", "/sdcard/screencap.png", ".",
+	).Run()
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "pull failed", http.StatusInternalServerError)
+		return
+	}
+	log.Println("OK")
+	http.ServeFile(w, r, "./screencap.png")
+}
